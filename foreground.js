4a3118f26eea56f82a85677b7c05781046a0ea0d
Welton Wang <weltonwang88@gmail.com>
2022-11-12 10:20:15 -0800

    test

--- foreground.js
+++ foreground.js
@@ -1,15 +1,19 @@
-[...document.querySelectorAll("article")].forEach(
-    (item) => {
-        var content = item.querySelector('div[lang="en"]>span').innerText;
-        fetch('https://api.moderatehatespeech.com/api/v1/moderate/', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ token: "test", text: content })
-        }).then(res => res.json())
-          .then(res => console.log(res));
+console.log("Running!");
 
-    }
-);
+window.onload = function () {
+    [...document.querySelectorAll("article")].forEach(
+        (item) => {
+            var content = item.querySelector('div[lang="en"]>span').innerText;
+            fetch('https://api.moderatehatespeech.com/api/v1/moderate/', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ token: "test", text: content })
+            }).then(res => res.json())
+                .then(res => console.log(res));
+
+        }
+    );
+}
