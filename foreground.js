02f03fd11b6e1d3ac654bea51717838671bad920
Welton Wang <weltonwang88@gmail.com>
2022-11-12 13:03:15 -0800

    changes

--- foreground.js
+++ foreground.js
@@ -1,43 +1,32 @@
 console.log("Running!");
 
-const config = { attributes: false, childList: true, subtree: false };
-
-const callback = (mutationList, observer) => {
-    for (const mutation of mutationList) {
-        if (mutation.type === 'childList') {
-            [...document.querySelectorAll("article")].forEach(
-                (item) => {
-                    if (!item.getAttribute("data-mod-status") == "true") {
-
-                        var content = item.querySelector('div[lang="en"]>span').innerText;
-                        fetch('https://api.moderatehatespeech.com/api/v1/twitter/', {
-                            method: 'POST',
-                            headers: {
-                                'Accept': 'application/json, text/plain, */*',
-                            },
-                            body: JSON.stringify({ text: content })
-                        }).then(res => res.json())
+function processTweets() {
+    [...document.querySelectorAll("article")].forEach(
+        (item) => {
+            if (item.getAttribute("data-mod-status") != "true") {
+                item.setAttribute("data-mod-status", "true");
+                var content = item.querySelector('div[lang="en"]>span');
+                if (content != null) {
+                    fetch('https://api.moderatehatespeech.com/api/v1/twitter/', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json, text/plain, */*',
+                        },
+                        body: JSON.stringify({ text: content.innerText })
+                    }).then(res => res.json())
                         .then((res) => {
-                            if (res["conf"] > 0.9 && res["class"] == "flag") {
-                                item.classList.add("mod-hidden");
+                            if (res["confidence"] > 0.9 && res["class"] == "flag") {
+                                item.setAttribute("hidden", "true");
+                                console.log("Hateful tweet detected!");
                             }
-                            console.log("Hateful tweet detected!");
-                            item.setAttribute("data-mod-status", "true");
                         });
-                    }
-
                 }
-            );
-        }
-    }
-};
+            }
 
-function startObserver() {
-    var targetNode = document.querySelector('div[aria-label="Timeline: Your Home Timeline"] > div');
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
+        }
+    );
 }
 
+setInterval(processTweets, 250);
 // observer.disconnect();
 
-document.addEventListener('DOMContentLoaded', startObserver);
